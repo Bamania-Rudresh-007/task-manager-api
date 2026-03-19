6a7f3d46b97f0af064b885b8f72cd934879e9b4f
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-19 12:31:34 +0530

    create a task model

---
+++ models/task.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: String,
+        description: String,
+        status: "pending" || "complete",
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    },
+    {timestamps: true}
+)
