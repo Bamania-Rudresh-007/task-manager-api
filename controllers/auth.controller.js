3b1a30a113afe50cfc33b31dcd6a77ab5c4591bf
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-19 13:32:08 +0530

    add the login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const signUp = async (req, res, next) => {
     try {
@@ -24,4 +25,37 @@ const signUp = async (req, res, next) => {
         })   
         next()
     }
-}   
+}  
+
+
+const login = async(req, res, next) => {
+
+    try{
+        const {email , password} = req.body;
+
+        const user = User.findOne({email});
+        const isMatch = bcrypt.compare(password, user.password);
+        
+        if(!user || !isMatch){
+            res.status(400).json({
+                message: "Invalid Email or password"
+            })
+        }
+
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_sECRET,
+            {expiresIn: "1h"}
+        )
+
+        res.json({
+            message: "Login Successfull",
+            token,
+        })
+    }catch(error){
+        next(error);
+    }
+}
+
+
+export {signUp, login}
