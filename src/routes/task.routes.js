5c2356065143cf3c9ec285535af17816d92be7fc
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-19 17:22:43 +0530

    add controllers to the routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-
+import {getAllTasks, getTaskById, createTask, updateTaskById, deleteTaskById} from "../controllers/user.controller"
 const taskRoutes = express.Router();
 
-taskRoutes.get("/", (req, res) => res.send("Get all tasks"));
-taskRoutes.post("/", (req, res) => res.send("Task created"));
-taskRoutes.get("/:id",  (req, res) => res.send(`specific task ${req.params.id}`));
-taskRoutes.patch("/:id", (req, res) => res.send(`Updating task ${req.params.id}`));
-taskRoutes.delete("/:id", (req, res) => res.send(`Deleting task ${req.params.id}`));
+taskRoutes.get("/", getAllTasks);
+taskRoutes.post("/", createTask);	
+taskRoutes.get("/:id", getTaskById);
+taskRoutes.patch("/:id", updateTaskById);
+taskRoutes.delete("/:id", deleteTaskById);
 
 export default taskRoutes;
+
