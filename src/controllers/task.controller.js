cd0968638fe60c07b8713283209bb0f9500f21f5
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-19 17:21:31 +0530

    Add the controllers for the taskRoutes

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,88 @@
+import Task from "../models/task.model.js";
+
+const getAllTasks = async(req, res) => {
+    try{
+        const allTasks = await Task.find();
+
+        res.json({
+            message: "fetched all tasks",
+            data: allTasks,
+        })
+
+    }catch(error){
+        res.status().jsoh({
+            message: "Failed fetching all Tasks",
+            error: error.message,
+        })
+    }
+}
+
+const getTaskById = async(req, res) => {
+    try{
+        const allTasks = await Task.findById(req.params.id);
+
+        res.json({
+            message: "fetched all tasks",
+            data: allTasks,
+        })
+
+    }catch(error){
+        res.status().jsoh({
+            message: "Failed fetching all Tasks",
+            error: error.message,
+        })
+    }
+}
+
+const createTask = async(req, res) => {
+    try {
+        console.log(req.body) 
+        const newTask = await Task.create(req.body);
+
+        res.json({
+            message: "Task created successfully",
+            data: newTask,
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "failed creating Task",
+            error: error.message,
+        })
+    }
+}
+
+const updateTaskById = async(req, res) => {
+
+    try{        
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {returnDocument: "after", runValidators: true});
+
+        res.json({
+            message: "Task updated successfully",
+            data: updatedTask,
+        })
+    }catch(error){
+        res.status(500).json({
+            message: "failed updating Task",
+            error: error.message,
+        })
+    }
+}
+
+const deleteTaskById = async(req, res) => {
+    try{        
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+
+        res.json({
+            message: "Task deleted successfully",
+            data: deletedTask,
+        })
+    }catch(error){
+        res.status(500).json({
+            message: "failed deleting  Task",
+            error: error.message,
+        })
+    }
+}
+
+export {getAllTasks, getTaskById, createTask, updateTaskById, deleteTaskById};
