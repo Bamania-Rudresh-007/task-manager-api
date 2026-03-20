bb177f219a723c43d041da8aeb5a746e640d5c6c
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-20 14:36:58 +0530

    fix the auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,18 +33,30 @@ const login = async(req, res, next) => {
     try{
         const {email , password} = req.body;
 
-        const user = User.findOne({email});
-        const isMatch = bcrypt.compare(password, user.password);
-        
-        if(!user || !isMatch){
-            res.status(400).json({
-                message: "Invalid Email or password"
+        if(!password){
+            return res.status(400).json({
+                message: "Password is required",
             })
         }
+        const user = await User.findOne({email});
 
+        if(!user){
+            return res.status(400).json({
+                    message: "User not found"
+                })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                meessage: "Invalid token, access denied",
+            })
+        }
+        
         const token = jwt.sign(
             {userId: user._id},
-            process.env.JWT_sECRET,
+            process.env.JWT_SECRET,
             {expiresIn: "1h"}
         )
 
