9036f32ca64e34239910b6ba1e02912deffafca0
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-21 10:29:40 +0530

    add the protect JWT authentication in tasks routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import {getAllTasks, getTaskById, createTask, updateTaskById, deleteTaskById} from "../controllers/task.controller.js"
+import protect from "../middlewares/auth.middlewares.js";
+
 const taskRoutes = express.Router();
 
-taskRoutes.get("/", getAllTasks);
-taskRoutes.post("/", createTask);	
-taskRoutes.get("/:id", getTaskById);
-taskRoutes.patch("/:id", updateTaskById);
-taskRoutes.delete("/:id", deleteTaskById);
+taskRoutes.get("/", protect, getAllTasks);
+taskRoutes.post("/", protect, createTask);
+taskRoutes.get("/:id", protect, getTaskById);
+taskRoutes.patch("/:id", protect, updateTaskById);
+taskRoutes.delete("/:id", protect, deleteTaskById);
 
 export default taskRoutes;
-
