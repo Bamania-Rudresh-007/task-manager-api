5cb978a0737bbf303a6df2e09198fc65a20009e2
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-21 10:27:12 +0530

    protect the task controller within the same user to operate only

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,88 +1,99 @@
 import Task from "../models/task.model.js";
 
-const getAllTasks = async(req, res) => {
-    try{
-        const allTasks = await Task.find();
-
-        res.json({
-            message: "fetched all tasks",
-            data: allTasks,
-        })
-
-    }catch(error){
-        res.status().jsoh({
-            message: "Failed fetching all Tasks",
-            error: error.message,
-        })
-    }
-}
-
-const getTaskById = async(req, res) => {
-    try{
-        const allTasks = await Task.findById(req.params.id);
-
-        res.json({
-            message: "fetched all tasks",
-            data: allTasks,
-        })
-
-    }catch(error){
-        res.status().jsoh({
-            message: "Failed fetching all Tasks",
-            error: error.message,
-        })
-    }
-}
-
-const createTask = async(req, res) => {
+const getAllTasks = async (req, res) => {
     try {
-        console.log(req.body) 
-        const newTask = await Task.create(req.body);
+        const allTasks = await Task.find({ user: req.user.userId });
+
+        res.json({
+            message: "fetched all tasks",
+            data: allTasks,
+        });
+    } catch (error) {
+        res.status().jsoh({
+            message: "Failed fetching all Tasks",
+            error: error.message,
+        });
+    }
+};
+
+const getTaskById = async (req, res) => {
+    try {
+        const allTasks = await Task.findById({
+            _id: req.params.id,
+            user: req.userId.id,
+        });
+
+        res.json({
+            message: "fetched all tasks",
+            data: allTasks,
+        });
+    } catch (error) {
+        res.status().jsoh({
+            message: "Failed fetching all Tasks",
+            error: error.message,
+        });
+    }
+};
+
+const createTask = async (req, res) => {
+    try {
+        console.log(req.body);
+        const newTask = await Task.create({
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            user: req.user.userId,
+        });
 
         res.json({
             message: "Task created successfully",
             data: newTask,
-        })
-
+        });
     } catch (error) {
         res.status(500).json({
             message: "failed creating Task",
             error: error.message,
-        })
+        });
     }
-}
+};
 
-const updateTaskById = async(req, res) => {
-
-    try{        
-        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {returnDocument: "after", runValidators: true});
+const updateTaskById = async (req, res) => {
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(
+            { _id: req.params.id, user: user.user.userId },
+            req.body,
+            { returnDocument: "after", runValidators: true },
+        );
 
         res.json({
             message: "Task updated successfully",
             data: updatedTask,
-        })
-    }catch(error){
+        });
+    } catch (error) {
         res.status(500).json({
             message: "failed updating Task",
             error: error.message,
-        })
+        });
     }
-}
+};
 
-const deleteTaskById = async(req, res) => {
-    try{        
-        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+const deleteTaskById = async (req, res) => {
+    try {
+        const deletedTask = await Task.findByIdAndDelete({
+            _id: req.params.id,
+            user: user.user.userId,
+        });
 
         res.json({
             message: "Task deleted successfully",
             data: deletedTask,
-        })
-    }catch(error){
+        });
+    } catch (error) {
         res.status(500).json({
             message: "failed deleting  Task",
             error: error.message,
-        })
+        });
     }
-}
+};
 
-export {getAllTasks, getTaskById, createTask, updateTaskById, deleteTaskById};
+export { getAllTasks, getTaskById, createTask, updateTaskById, deleteTaskById };
