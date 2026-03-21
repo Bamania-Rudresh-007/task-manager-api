60ed377ada415589de003ff7127f5bfbaa29e731
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-21 18:25:08 +0530

    add the connectioon between frontend and backend

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import express from "express"
 import taskRoutes from "./src/routes/task.routes.js";
 import connectDb from "./src/db/task.db.js";
 import authRoutes from "./src/routes/auth.routes.js";
+import cors from "cors";
+
 
 const app = express()
 const port = process.env.PORT || 7300;
 
+app.use(cors())
 app.use(express.json())
-
 app.use("/api/tasks", taskRoutes);
 app.use("/api/auth", authRoutes);
 
